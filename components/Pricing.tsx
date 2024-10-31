e024105ad654910349c3ec6e7fa79dc382808983
Rohit lodhi <142474520+Rohitlodhii@users.noreply.github.com>
2024-10-31 12:34:10 +0530

    Update Pricing.tsx
    Bug fixes eslint error

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,7 +1,7 @@
 import { Badge } from "@/components/ui/badge"
 
 import { Card, CardContent,  CardHeader, CardTitle } from "@/components/ui/card"
-import { MarqueeDemo } from "./Review"
+
 import ContactUs from "./ContactUs"
 import BlurFade from "./ui/blur-fade"
 
